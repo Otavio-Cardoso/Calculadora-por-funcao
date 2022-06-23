dbe76005c4210a0cd216d98c15920e85d47b0ecb
curso.ads <curso.ads@LAB06-72>
2022-06-22 21:01:58 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora por funcao/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_por_funcao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma;
+
+            soma = Calcular(30, 20, "+");
+            Console.WriteLine(soma);
+            Console.ReadKey();
+
+        }
+        static public int Calcular(int numero1, int numero2, string operacao)
+        {
+            int resultado = 0;
+            if (operacao == "+")
+            {
+                resultado = numero1 + numero2;
+            }
+            else if (operacao == "*")
+            {
+                resultado = numero1 * numero2;
+            }
+            else if (operacao == "/")
+            {
+                resultado = numero1 / numero2;
+            }
+            else if (operacao == "-")
+            {
+                resultado = numero1 - numero2;
+            }
+
+            return resultado;
+        }
+    
+    }
+}
